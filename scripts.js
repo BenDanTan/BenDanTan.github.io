676544fd7fe672b2913cf3522ee47b2050b34b71
Ben Tan <bendantan@gmail.com>
2019-12-29 00:12:36 -0600

    Index styling

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidenav").style.width = "425px";
+    document.getElementById("page").style.marginLeft = "425px";
+  }
+  
+  function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("page").style.marginLeft= "0";
+  }
