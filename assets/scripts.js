ec9e337614844e4e9aaeda6eb61ef5b85a8665cc
Ben Tan <bendantan@gmail.com>
2020-01-11 00:35:03 -0600

    More restyling of home page

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,6 +11,19 @@ function closeNav() {
 function loadPage() {
   document.getElementById("titleText").style.opacity = "1";
   document.getElementById("titleImage").style.opacity = "1";
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+       document.getElementById("page").style.backgroundColor = "#121212"
+       document.getElementById("header").style.opacity = "0"
+       $('.bubble-text').css('background-color','#333333')
+    }
+    if ($(this).scrollTop() < 100) {
+       document.getElementById("page").style.backgroundColor = "#333333"
+       document.getElementById("header").style.opacity = "1"
+       $('.bubble-text').css('background-color','#121212')
+    }
+ })
 }
 
 function hoverBubble(title) {
@@ -22,5 +35,6 @@ function unhoverBubble(title) {
 }
 
 function clickBubble(title) {
-  window.location = title + "/index.html"
+  window.location = "pages/" + title +"/index.html"
 }
+
